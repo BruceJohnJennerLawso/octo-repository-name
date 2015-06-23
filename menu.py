73a5077a1e086a98703c2cdf074c9089779831a0
bruce <johnnybgoode@rogers.com>
2015-06-23 13:57:00 -0400

    yay easy

--- menu.py
+++ menu.py
@@ -3,7 +3,8 @@
 ## like Cmd++ but infinitely easier and better #################################
 ################################################################################
 
-
+def caseInsensitivematch(value1, value2):
+	return False;
 
 def printMenuHeader(style_char, menu_name):
 	print ((2*style_char) + (menu_name)+ (2*style_char));
@@ -13,6 +14,27 @@ class dynMenu:
 	def __init__(self, menu_name, menu_id):
 		self.menuName = menu_name;
 		self.menuId = menu_id;
+		menuList.append(self);
 		
-	def Menu(last_input):
+	menuList = [];	
 		
+	def Menu(last_input, prompt):
+		if((last_input == "q")or(last_input == "Q")):
+			return "q";
+		else:
+			i = "notqorQ";
+			while((i != "q")and(i !="Q")):
+				i = raw_input(prompt);
+				
+				for cy in range(0, len(menuList)):
+					if(menuList[cy].matchId(i) == True):
+						
+					
+					
+	def matchId(i):
+		return caseInsensitivematch(self.menuId, i);
+			
+					
+					
+					
+					
